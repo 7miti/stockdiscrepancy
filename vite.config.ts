1d5fe9347d1399b351715014ac53eb12cb7d419f
7miti <cpahmiti@gmail.com>
2026-05-11 22:01:24 +0100

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/your-repository-name/', // <--- ADD THIS LINE (e.g., '/shoe-scanner/')
+    base: '/stockdiscrepancy/', // <--- ADD THIS LINE (e.g., '/shoe-scanner/')
     plugins:[react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
